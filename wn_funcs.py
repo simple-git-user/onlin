4a0f2c82e76dcb1b4e055947323458c9aa3d97d4
unknown <VishwanathY@inlttools5.in.nds.com>
2013-05-15 13:44:42 +0530

    corrected get_all_synonyms

--- wn_funcs.py
+++ wn_funcs.py
@@ -1,9 +1,11 @@
+import nltk
+from nltk.corpus import wordnet as wn
 
 def get_all_synonyms(word):
     """gets in a list all synonyms of all the synsets of word"""
     l_syns = []
     synsets = wn.synsets(word)
     for synset in synsets:
-        if word2 in synset.lemma_names:
-            l_syns.append(  word2 )
+        for lemma_name in synset.lemma_names:
+            l_syns.append(  lemma_name )
     return l_syns
