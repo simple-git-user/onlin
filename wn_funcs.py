59c2c39f50bf46c11d2a42f32de49757afeaf687
simple-git-user <vishwanathsy@gmail.com>
2013-05-16 14:00:16 +0530

    permutations

--- wn_funcs.py
+++ wn_funcs.py
@@ -1,5 +1,6 @@
 import nltk
 from nltk.corpus import wordnet as wn
+import itertools as itt
 
 def get_all_synonyms(word):
     """gets in a list all synonyms of all the synsets of word"""
@@ -9,3 +10,18 @@ def get_all_synonyms(word):
         for lemma_name in synset.lemma_names:
             l_syns.append(  lemma_name )
     return l_syns
+	
+def get_all_antonyms(word):
+	"""get in a list all antonyms of all synsets of word"
+	l_antonyms = []
+    synsets = wn.synsets(word)
+    for synset in synsets:
+        for lemma in synset.lemmas:
+            l_antonyms.extend(  lemma.antonyms() )
+    return l_antonyms
+
+#AxB
+#itt.product([0,1,2],[1,2])
+#(1,2),(2,1)...
+#itt.permutations([1,2,3],2)
+
